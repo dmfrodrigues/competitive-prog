c933b07f257c32043e3c4daf98e600bd016564ba
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-05 21:26:54 +0000

    Faster to calculate catalan

--- SWERC/SWERC19/J/J.cpp
+++ SWERC/SWERC19/J/J.cpp
@@ -15,22 +15,35 @@ typedef pair<PII,PII>           PPP;
 #define Y second
 const lli INF = 1000000000;
 const lli MOD = 1000000007;
+// 10^9+7; prime
 
-VI CATALAN(1000, 0);
-void init_catalan() {
-    CATALAN[0] = 1;
-    FOR(n,0,CATALAN.size()-1)
-        FOR(i,0,n+1)
-            CATALAN[n+1] = (CATALAN[n+1] + CATALAN[i]*CATALAN[n-i])%MOD;
+PII find_bezout(lli a, lli b){
+    if(b == 0) return PII(1,0);
+    PII u = find_bezout(b, a%b);
+    return PII(u.Y, u.X-(a/b)*u.Y);
 }
 
+lli inv(lli a, lli MOD){
+    return find_bezout(a, MOD).X%MOD;
+}
+
+lli factorial(lli n, lli MOD){
+    lli ret = 1;
+    FOR(i,1,n+1) ret = (ret*i)%MOD;
+    return ret;
+}
+
+lli catalan(lli n, lli MOD){
+    lli ret = factorial(2*n, MOD)*inv(factorial(n+1, MOD)*factorial(n, MOD), MOD);
+    return (ret%MOD + MOD)%MOD;
+}
 
 lli solve(const VI::const_iterator &begin, const VI::const_iterator &end){
     if(begin == end) return 1;
 
     lli m = *min_element(begin, end);
     lli n = count(begin, end, m);
-    lli ret = CATALAN[n]%MOD;
+    lli ret = catalan(n, MOD);
 
     VI::const_iterator left = begin;
     VI::const_iterator right;
@@ -49,8 +62,6 @@ lli solve(const VI::const_iterator &begin, const VI::const_iterator &end){
 }
 
 int main(){
-    init_catalan();
-
     // INPUT
     lli N; cin >> N;
     VI v(N);
