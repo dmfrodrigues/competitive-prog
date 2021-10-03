d7e96583704d340d18e5e5a533e2e8695e12012d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-10-03 17:44:12 +0100

    USACO fence

--- USACO/CH3/S3/fence/fence.cpp
+++ USACO/CH3/S3/fence/fence.cpp
@@ -20,28 +20,78 @@ typedef pair<PII,PII>           PPP;
 #define Y second
 const lli INF = 1000000000;
 
-VI euler_circuit(vector<multiset<lli>> &adj, lli u){
-    while(!adj[u].empty()){
-        lli v = *adj[u].begin();
-        adj[u].erase(adj[u].find(v));
-        adj[v].erase(adj[v].find(u));
-        euler_circuit(adj, v);
+VI euler_circuit(vector<multiset<lli>> adj, lli source){
+    stack<lli> call_stack;
+    VI circuit;
+    call_stack.push(source);
+    while(!call_stack.empty()){
+        lli u = call_stack.top();
+        if(!adj[u].empty()){
+            lli v = *adj[u].begin();
+            adj[u].erase(adj[u].find(v));
+            adj[v].erase(adj[v].find(u));
+            call_stack.push(v);
+        } else {
+            circuit.push_back(u);
+            call_stack.pop();
+        }
     }
-    
+    reverse(ALL(circuit));
+    return circuit;
 }
+
+/*
+class euler_circuit{
+    vector<multiset<lli>> adj;
+    stack<lli> call_stack;
+    VI circuit;
+public:
+    euler_circuit(vector<multiset<lli>> adj_):adj(adj_){}
+    VI compute(lli u){
+        call_stack.push(u);
+        while(!call_stack.empty()){
+            u = call_stack.top();
+            if(!adj[u].empty()){
+                lli v = *adj[u].begin();
+                adj[u].erase(adj[u].find(v));
+                adj[v].erase(adj[v].find(u));
+                call_stack.push(v);
+            } else {
+                circuit.push_back(u);
+                call_stack.pop();
+            }
+        }
+        reverse(ALL(circuit));
+        return circuit;
+    }
+};
+*/
 
 int main(){
     ifstream fin("fence.in");
     ofstream fout("fence.out");
     
     ///INPUT
-    
+    int F; fin >> F;
+    vector<multiset<lli>> adj(500);
+    int u, v;
+    FOR(i,0,F){
+        fin >> u >> v;
+        adj[u-1].insert(v-1);
+        adj[v-1].insert(u-1);
+    }
 
     ///PROCESSING
-    
+    lli source = 0;
+    FOR(i,0,adj.size()) if(adj[i].size() % 2 == 1){
+        source = i;
+        break;
+    }
+
+    VI ret = euler_circuit(adj, source);
     
     ///OUTPUT
-    
+    for(const int &u: ret) fout << u+1 << endl;
 
     return 0;
 }
